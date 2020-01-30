3755333810b5a3050cea7b2ddfdcd61e2284edca
JayKhakhariya <jaykhakhariya40@gmail.com>
2020-01-30 14:22:41 -0600

    Created Register.java

---
+++ app/src/main/java/com/sicknasty/application/Register.java
@@ -0,0 +1,14 @@
+package com.sicknasty.application;
+
+public class Register {
+    private String userName,password;
+
+    public Register(String userName,String password){
+        this.password=password;
+        this.userName=userName;
+    }
+    public void addToDatabase(){
+
+    }
+
+}
