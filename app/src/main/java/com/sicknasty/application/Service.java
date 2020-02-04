0a7fdc0236fae71117742aad548684a13fdb755f
aaron_salo <saloamj@myumanitoba.ca>
2020-02-04 16:46:47 -0600

    Updated the GetPosts class GetPosts is essentially a wrapper for the db, acts as a method of passing things between UI layer and business layer

--- app/src/main/java/com/sicknasty/application/Service.java
+++ app/src/main/java/com/sicknasty/application/Service.java
@@ -2,13 +2,21 @@ package com.sicknasty.application;
 
 import com.sicknasty.persistence.stubs.*;
 
+
+/*******
+ * @author aaron
+ *
+ * this class manages the database, and allows us to fetch the db from other classes with ease
+ */
 public class Service {
 
     private static UserPersistenceStub userData = null;
     private static PostPersistenceStub postData = null;
     private static PagePersistenceStub pageData = null;
 
+    //this will retrieve the user stub, from which we can call functs such as getUser()
     public static synchronized UserPersistenceStub getUserData() {
+        //if the stub hasn't been created yet, create it
         if(userData == null) {
             userData = new UserPersistenceStub();
         }
