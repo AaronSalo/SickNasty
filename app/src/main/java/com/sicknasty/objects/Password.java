f5efbbcdb8c7102830fa3b0bebe8e57ff5074a7a
aaron_salo <saloamj@myumanitoba.ca>
2020-02-04 12:23:46 -0600

    Changes to user and password class Also added a manager template class, needs further modifications

--- app/src/main/java/com/sicknasty/objects/Password.java
+++ app/src/main/java/com/sicknasty/objects/Password.java
@@ -1,12 +1,17 @@
 package com.sicknasty.objects;
 
-
+//@project sicknasty
+//@author aaron salo
+//this password object handles all password related stuff
+//All hashing functions and password validation is done here
 public class Password {
 
-    private String hash;
+    private String hash; //a hashed version of the password
     private String salt; //a new "salt" will be created for each user, just makes things more secure
 
     private int maxPassLength = 30;
+
+
     public Password(String password) {
         salt = generateSalt(); //generate the salt for this obj
         hash = hash(password, salt);
@@ -24,14 +29,14 @@ public class Password {
         }
     }
 
-    //todo
+    //todo -- lucas has a good idea on how to implement this so im leaving it to him
     private static String generateSalt(){
-
+        return "";
     }
 
 
     //if the stored hash equals the hash of the input, the password is correct
-    public boolean checkPass(String input) {
-        return hash.equals(hash(input, salt));
+    public boolean checkPass(String inputPassword) {
+        return hash.equals(hash(inputPassword, salt));
     }
 }//end of class
