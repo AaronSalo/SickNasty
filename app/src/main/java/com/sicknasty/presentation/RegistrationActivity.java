993c22daa32e8dcc6de5436eb465df10c54bf029
aaron_salo <saloamj@myumanitoba.ca>
2020-02-04 15:52:35 -0600

    changes to MainActivity and RegistrationActivity Added some logic for creating a user in registration activity

--- app/src/main/java/com/sicknasty/presentation/RegistrationActivity.java
+++ app/src/main/java/com/sicknasty/presentation/RegistrationActivity.java
@@ -3,6 +3,9 @@ package com.sicknasty.presentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.sicknasty.objects.User;
 
 import com.sicknasty.R;
 
@@ -12,5 +15,20 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        //get the sign up details from the ui
+        String name = findViewById(R.id.signUpName).toString();
+        String username = findViewById(R.id.signUpUsername).toString();
+        String password = findViewById(R.id.signUpPassword).toString();
+
+        //validate the new account and create it
+        User newUser = signUpValidation(name, username, password);
+    }
+
+    private User signUpValidation(String name, String username, String password) {
+        if(name != null) {
+            
+        }
+        return null;
     }
 }
