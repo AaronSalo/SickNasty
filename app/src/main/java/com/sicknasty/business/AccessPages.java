f6998dea1d275cb14e4dd69f02e8b5c9c20efbea
aaron_salo <saloamj@myumanitoba.ca>
2020-02-04 17:23:30 -0600

    added AccessPages, a wrapper for the db acts as a bridge betwen the db and the ui

--- app/src/main/java/com/sicknasty/business/AccessPages.java
+++ app/src/main/java/com/sicknasty/business/AccessPages.java
@@ -1,4 +1,48 @@
 package com.sicknasty.business;
 
+import com.sicknasty.application.Service;
+import com.sicknasty.objects.Page;
+import com.sicknasty.persistence.stubs.PagePersistenceStub;
+
+/** @author aaron
+ * wrapper for the page db
+ * passes info from UI to the db and vise versa
+ */
+
 public class AccessPages {
-}
+
+    private PagePersistenceStub pageHandler;
+    private Page page;
+
+    public AccessPages() {
+        pageHandler = Service.getPageData();
+    }
+
+    /**
+     * @param pageName name of the page we want to get
+     * @return the page, or null if not found
+     */
+    public Page getPage(String pageName) {
+        return pageHandler.GetPage(pageName);
+    }
+
+    /**
+     * Inserts a new Page.
+     *
+     * @return      returns true on success, otherwise return false
+     */
+    public boolean InsertNewPage(Page page){
+        return pageHandler.InsertNewPage(page);
+    }
+
+    /**
+     * Delete a Page by its unique name.
+     *
+     * @param   name  the unique name of the Page
+     * @return      returns true if it deleted successfully, otherwise false
+     */
+    public boolean DeletePage(String name){
+        return pageHandler.DeletePage(name);
+    }
+
+}//end of class
