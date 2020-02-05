a727bc959e0b5b0fd631c7ec895cc279ae32d4fd
aaron_salo <saloamj@myumanitoba.ca>
2020-02-05 15:37:54 -0600

    Refactoring CommunityPage deleted redundant Community class and merged it into CommunityPage

--- app/src/main/java/com/sicknasty/objects/CommunityPage.java
+++ app/src/main/java/com/sicknasty/objects/CommunityPage.java
@@ -1,14 +1,28 @@
 package com.sicknasty.objects;
 
-public class CommunityPage extends Page {
-    private Community community;
+import java.util.ArrayList;
 
-    public CommunityPage(String name, Community community){
-        super(name);
-        this.community = community;
+public class CommunityPage extends Page {
+
+    private ArrayList<User> users; //list of users that are a part of this page
+
+    public CommunityPage(String name, User creator){
+        super(name, creator);
+        this.users = new ArrayList<User>();
+        users.add(creator);
     }
 
-    public Community getCommunity() {
-        return community;
+
+    public void addUser(User newUser){
+        users.add(newUser);
+    }
+
+    
+    /**
+     * @param user  user we want to delete
+     * @return  true on success, false on failure
+     */
+    public boolean deleteUser(User user) {
+        return users.remove(user);
     }
 }
