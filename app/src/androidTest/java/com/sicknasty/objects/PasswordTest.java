fae1c335fd9f60fa18a3a25c147e077f4f03624b
JaiSandhu <sandhuj6@myumanitoba.ca>
2020-02-05 15:46:00 -0600

    inserting password test into correct folder

---
+++ app/src/androidTest/java/com/sicknasty/objects/PasswordTest.java
@@ -0,0 +1,16 @@
+package com.sicknasty.objects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordTest {
+
+    @Test
+    public void changePassword() {
+    }
+
+    @Test
+    public void checkPass() {
+    }
+}
