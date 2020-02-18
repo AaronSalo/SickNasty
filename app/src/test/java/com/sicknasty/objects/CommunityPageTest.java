39e42d27433108ac2b58874e232be8e602636a05
JaiSandhu <sandhuj6@myumanitoba.ca>
2020-02-18 12:30:16 -0600

    community page updates

---
+++ app/src/test/java/com/sicknasty/objects/CommunityPageTest.java
@@ -0,0 +1,36 @@
+package com.sicknasty.objects;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommunityPageTest {
+
+    @Test
+    public void addPost() {
+    }
+
+    @Test
+    public void deletePost() {
+    }
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void addMod() {
+    }
+
+    @Test
+    public void getComName() {
+    }
+
+    @Test
+    public void banUser() {
+    }
+
+    @Test
+    public void deleteUser() {
+    }
+}
