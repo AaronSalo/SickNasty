174f530641f7cf0d326fd25f3075f89bfa3b6f69
Lucas Huynh <huynhl@myumanitoba.ca>
2020-02-25 15:13:06 -0600

    Added PageFollowers table and changed Posts table details

--- app/src/main/java/com/sicknasty/persistence/sql/HSQLDBInitializer.java
+++ app/src/main/java/com/sicknasty/persistence/sql/HSQLDBInitializer.java
@@ -26,8 +26,9 @@ class HSQLDBInitializer {
                 "likes INTEGER NOT NULL," +
                 "dislikes INTEGER NOT NULL," +
                 "creator_username VARCHAR(32) NOT NULL," +
-                "time_created LONG NOT NULL" +
-                "type TINYINT NOT NULL" +
+                "time_created BIGINT NOT NULL," +
+                "type TINYINT NOT NULL," +
+                "FOREIGN KEY(creator_username) REFERENCES Users(username) ON DELETE CASCADE" +
             ")"
         );
         stmt.execute();
@@ -45,8 +46,18 @@ class HSQLDBInitializer {
             "CREATE TABLE IF NOT EXISTS PagePosts (" +
                 "p_id INTEGER NOT NULL," +
                 "pg_name VARCHAR(32) NOT NULL," +
-                "FOREIGN KEY(p_id) REFERENCES Posts(p_id) ON CASCADE DELETE," +
-                "FOREIGN KEY(pg_name) REFERENCES Pages(pg_name) ON CASCADE DELETE" +
+                "FOREIGN KEY(p_id) REFERENCES Posts(p_id) ON DELETE CASCADE," +
+                "FOREIGN KEY(pg_name) REFERENCES Pages(pg_name) ON DELETE CASCADE" +
+            ")"
+        );
+        stmt.execute();
+
+        stmt = db.prepareStatement(
+            "CREATE TABLE IF NOT EXISTS PageFollowers (" +
+                "username VARCHAR(32) NOT NULL," +
+                "pg_name VARCHAR(32) NOT NULL," +
+                "FOREIGN KEY(username) REFERENCES Users(username) ON DELETE CASCADE," +
+                "FOREIGN KEY(pg_name) REFERENCES Pages(pg_name) ON DELETE CASCADE" +
             ")"
         );
         stmt.execute();
