c2f60d74ef35e34de6201bd28ce451d8fc16a32a
JaiSandhu <sandhuj6@myumanitoba.ca>
2020-03-18 13:59:06 -0500

    adding messages and clean code a bit

--- app/src/main/java/com/sicknasty/presentation/MessageActivity.java
+++ app/src/main/java/com/sicknasty/presentation/MessageActivity.java
@@ -33,13 +33,14 @@ public class MessageActivity extends AppCompatActivity {
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
+
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_chat);
-
             Intent intent=getIntent();
-
             Bundle extras;                  //useds to get current user and logged in user from previous activity
             extras = intent.getExtras();
+
+
 
             try {                              //gets current user (person being messaged) and puts it into curUser var
                 curUser = users.getUser(extras.getString("currentUser"));
@@ -48,6 +49,8 @@ public class MessageActivity extends AppCompatActivity {
                 String errorMsg = e.getMessage();
                 Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
             }
+
+
 
             try {                               //gets Logged in user and puts it into curUser var
                 loggedInUser = users.getUser(extras.getString("loggedInUser"));
@@ -59,11 +62,14 @@ public class MessageActivity extends AppCompatActivity {
 
 
 
+
             final ListView lvMessages = (ListView) findViewById(R.id.messages_view);      //lists messages
             TextView chatName = findViewById(R.id.chatname);             //chatname at the top of each chat
             ImageButton sendButton = findViewById(R.id.sendMessage);     //button that will send the message after entered
             final EditText message = findViewById(R.id.messageEntered);        //message to be sent to the listview
             chatName.setText(curUser.getName());                            //implements chatname
+
+
 
 
             //creates custom adapter so message text displays speacial way
@@ -72,14 +78,13 @@ public class MessageActivity extends AppCompatActivity {
             sendButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
                     String messageinput = message.getText().toString();
+
                     if(validateMsg(messageinput)) {
 
                         try{
                             Message message1 = new Message(messageinput,loggedInUser,curUser);
                             users.addMessage(message1);
-
                             messageAdapter = new MessageAdapter(MessageActivity.this, users.getMessages(loggedInUser,curUser),loggedInUser, curUser);
 
                             Log.d("messageAdapter problem", message.toString());
@@ -87,10 +92,12 @@ public class MessageActivity extends AppCompatActivity {
                             lvMessages.setAdapter(messageAdapter);             //adapter set here so list view updates right after a message is sent
 
                             messageAdapter.notifyDataSetChanged();
+
                             lvMessages.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);       //used to make the added message at the bottom of the list
-                                                                                                        // view and scrolls up when a new item is added
-                            lvMessages.setStackFromBottom(true);
+                            lvMessages.setStackFromBottom(true);                                        // view and scrolls up when a new item is added
+
                             message.getText().clear();                      //clears message edittext when message is entered
+
 
                         }catch (MessageException e){
 
@@ -101,13 +108,16 @@ public class MessageActivity extends AppCompatActivity {
                     }
                 }
             });
-            lvMessages.setAdapter(messageAdapter);             //adapter to show messages in array list from database,
-                                                                // here so if message isnt added. messages still show up
-            messageAdapter.notifyDataSetChanged();
+
+            lvMessages.setAdapter(messageAdapter);             //this code needs to be repeated so all the messages show up and are scrolled to the very bottom.
+            messageAdapter.notifyDataSetChanged();             //if not, then it is just a blank list view until a new message is input and then all the previous
+                                                               //messages show up
+
+
 
             lvMessages.setTranscriptMode(ListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);       //used to make the added message at the bottom of the list
-                                                                                        // view and scrolls up when a new item is added
-            lvMessages.setStackFromBottom(true);
+            lvMessages.setStackFromBottom(true);                                        // view and scrolls up when a new item is added
+
 
 
 
@@ -122,9 +132,11 @@ public class MessageActivity extends AppCompatActivity {
             String returnText = "";
             message = message.trim();
 
+
             if(message.isEmpty()){
                 returnText = "Message cannot have 0 characters";
                 returnVal = false;
+
 
             }
             if(message.length()>255){
@@ -132,10 +144,12 @@ public class MessageActivity extends AppCompatActivity {
                 returnVal = false;
             }
 
+
             if(!returnVal) {
                 Toast toast = Toast.makeText(MessageActivity.this, returnText, Toast.LENGTH_SHORT);
                 toast.show();
             }
+
             return returnVal;
         }
 
@@ -144,6 +158,7 @@ public class MessageActivity extends AppCompatActivity {
         goToHome();
     }
 
+    
     private void goToHome(){
         if(curUser!=null){
             Intent intent=new Intent(MessageActivity.this,PageActivity.class);
