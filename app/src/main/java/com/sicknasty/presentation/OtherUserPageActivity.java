cf42199f66012a3a2fe64cc96f8d773e22c17f23
JaiSandhu <sandhuj6@myumanitoba.ca>
2020-03-28 16:07:07 -0500

    adding cleanup

--- app/src/main/java/com/sicknasty/presentation/OtherUserPageActivity.java
+++ app/src/main/java/com/sicknasty/presentation/OtherUserPageActivity.java
@@ -3,14 +3,12 @@ package com.sicknasty.presentation;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sicknasty.R;
 import com.sicknasty.business.AccessPages;
