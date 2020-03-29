5545d2014e3196bc200d2de50d475f127e2b995b
JayKhakhariya <jaykhakhariya40@gmail.com>
2020-03-28 20:15:32 -0500

    follow button functionality added

--- app/src/main/java/com/sicknasty/presentation/OtherUserPageActivity.java
+++ app/src/main/java/com/sicknasty/presentation/OtherUserPageActivity.java
@@ -21,6 +21,7 @@ import com.sicknasty.objects.Exceptions.UserNotFoundException;
 import com.sicknasty.objects.Page;
 import com.sicknasty.objects.User;
 import com.sicknasty.persistence.exceptions.DBPageNameNotFoundException;
+import com.sicknasty.persistence.exceptions.DBUserAlreadyFollowingException;
 import com.sicknasty.persistence.exceptions.DBUsernameNotFoundException;
 import com.sicknasty.presentation.adapter.PostAdapter;
 
@@ -30,12 +31,14 @@ public class OtherUserPageActivity extends AppCompatActivity {
     AccessPages pages = new AccessPages();
     AccessPosts posts = new AccessPosts();
     SharedPreferences sharedPreferences;
+    Page page;
+    User loggedUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_page);
 
-        Button followButton = findViewById(R.id.followButton);
+        final Button followButton = findViewById(R.id.followButton);
         Button messageButton = findViewById(R.id.messageButton);
         ListView listView = findViewById(R.id.lvOtherPost);
         Button homeButton = findViewById(R.id.home);
@@ -44,14 +47,16 @@ public class OtherUserPageActivity extends AppCompatActivity {
         Intent intent = getIntent();
         final String userName = intent.getStringExtra("user");
         String message;
+
         sharedPreferences = getSharedPreferences("MY_PREFS", MODE_PRIVATE);
-        final String loggedInUser = sharedPreferences.getString("username",null);       //this is necessary for message
+        final String loggedInUsername = sharedPreferences.getString("username",null);       //this is necessary for message
 
         PostAdapter postAdapter = null;
         try {
-            Page page = pages.getPage(userName);        //again ,remember pageName is Username
+            loggedUser = users.getUser(loggedInUsername);
+            page = pages.getPage(userName);        //again ,remember pageName is Username
             postAdapter = new PostAdapter(this, R.layout.activity_post, posts.getPostsByPage(page));
-        } catch (DBPageNameNotFoundException | NoValidPageException e) {
+        } catch (DBPageNameNotFoundException | NoValidPageException | UserNotFoundException | DBUsernameNotFoundException e) {
             message = e.getMessage();
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         }
@@ -61,17 +66,16 @@ public class OtherUserPageActivity extends AppCompatActivity {
 
 
         //also set all the things we did in userPage(followers ,following and posts)
-        try {
-            User thisUser = users.getUser(userName);
-        } catch (UserNotFoundException | DBUsernameNotFoundException e) {
-            message =  e.getMessage();
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-        }
-
         followButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
+                    pages.addFollower(page,loggedUser);
+                    followButton.setText("Following");
 
+                } catch (DBUserAlreadyFollowingException e) {
+                    Toast.makeText(OtherUserPageActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -80,7 +84,7 @@ public class OtherUserPageActivity extends AppCompatActivity {
             public void onClick(View v) {
                     try {
                         Intent startIntent=new Intent(OtherUserPageActivity.this, LoggedUserPageActivity.class);
-                        startIntent.putExtra("user", loggedInUser);
+                        startIntent.putExtra("user", loggedInUsername);
                         startActivity(startIntent);
                         finish();
 
@@ -94,7 +98,7 @@ public class OtherUserPageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent newIntent = new Intent(OtherUserPageActivity.this,MessageActivity.class);
-                newIntent.putExtra("loggedInUser", loggedInUser);       //sends whos logged in to message activity
+                newIntent.putExtra("loggedInUser", loggedInUsername);       //sends whos logged in to message activity
                 newIntent.putExtra("currentUser", userName);            //sends the person being sent the message to message activity
                 startActivity(newIntent);
                 finish();
