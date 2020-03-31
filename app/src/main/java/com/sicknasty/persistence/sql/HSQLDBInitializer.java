4b99ce33761dbbc7015fbbbf66a9766a2fdc6c86
JayKhakhariya <jaykhakhariya40@gmail.com>
2020-03-31 00:25:35 -0500

    wrote some more unit tests

--- app/src/main/java/com/sicknasty/persistence/sql/HSQLDBInitializer.java
+++ app/src/main/java/com/sicknasty/persistence/sql/HSQLDBInitializer.java
@@ -14,96 +14,96 @@ class HSQLDBInitializer {
         Log.d("INNER SQL", "Setting up tables");
 
         PreparedStatement stmt = db.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS Users (" +
-                "username VARCHAR(32) PRIMARY KEY," +
-                "name VARCHAR(32) NOT NULL," +
-                "password VARCHAR(128) NOT NULL" +
-            ")"
-        );
-        stmt.execute();
-        
-        stmt = db.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS Posts (" +
-                "p_id INTEGER IDENTITY PRIMARY KEY," +
-                "text VARCHAR(1024) NOT NULL," +
-                "media_path VARCHAR(1024)," +
-                "likes INTEGER NOT NULL," +
-                "dislikes INTEGER NOT NULL," +
-                "creator_username VARCHAR(32) NOT NULL," +
-                "time_created BIGINT NOT NULL," +
-                "FOREIGN KEY(creator_username) REFERENCES Users(username)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE" +
-            ")"
-        );
-        stmt.execute();
-        
-        stmt = db.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS Pages (" +
-                "pg_name VARCHAR(32) NOT NULL PRIMARY KEY," +
-                "creator_username VARCHAR(32) NOT NULL," +
-                "type TINYINT NOT NULL," +
-                "FOREIGN KEY(creator_username) REFERENCES Users(username)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE" +
-            ")"
-        );
-        stmt.execute();
-            
-        stmt = db.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS PagePosts (" +
-                "p_id INTEGER NOT NULL," +
-                "pg_name VARCHAR(32) NOT NULL," +
-                "FOREIGN KEY(p_id) REFERENCES POSTS(p_id) ON DELETE CASCADE," +
-                "FOREIGN KEY(pg_name) REFERENCES Pages(pg_name)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE" +
-            ")"
+                "CREATE TABLE IF NOT EXISTS Users (" +
+                        "username VARCHAR(32) PRIMARY KEY," +
+                        "name VARCHAR(32) NOT NULL," +
+                        "password VARCHAR(128) NOT NULL" +
+                        ")"
         );
         stmt.execute();
 
         stmt = db.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS PageFollowers (" +
-                "username VARCHAR(32) NOT NULL," +
-                "pg_name VARCHAR(32) NOT NULL," +
-                "FOREIGN KEY(username) REFERENCES Users(username)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE," +
-                "FOREIGN KEY(pg_name) REFERENCES Pages(pg_name)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE" +
-            ")"
+                "CREATE TABLE IF NOT EXISTS Posts (" +
+                        "p_id INTEGER IDENTITY PRIMARY KEY," +
+                        "text VARCHAR(1024) NOT NULL," +
+                        "media_path VARCHAR(1024)," +
+                        "likes INTEGER NOT NULL," +
+                        "dislikes INTEGER NOT NULL," +
+                        "creator_username VARCHAR(32) NOT NULL," +
+                        "time_created BIGINT NOT NULL," +
+                        "FOREIGN KEY(creator_username) REFERENCES Users(username)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE" +
+                        ")"
         );
         stmt.execute();
 
         stmt = db.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS Messages (" +
-                "sender VARCHAR(32) NOT NULL," +
-                "receiver VARCHAR(32) NOT NULL," +
-                "message VARCHAR(1024) NOT NULL," +
-                "time_sent BIGINT NOT NULL," +
-                "time_seen BIGINT," +
-                "FOREIGN KEY(sender) REFERENCES Users(username)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE," +
-                "FOREIGN KEY(receiver) REFERENCES Users(username)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE" +
-            ")"
+                "CREATE TABLE IF NOT EXISTS Pages (" +
+                        "pg_name VARCHAR(32) NOT NULL PRIMARY KEY," +
+                        "creator_username VARCHAR(32) NOT NULL," +
+                        "type TINYINT NOT NULL," +
+                        "FOREIGN KEY(creator_username) REFERENCES Users(username)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE" +
+                        ")"
         );
         stmt.execute();
 
         stmt = db.prepareStatement(
-            "CREATE TABLE IF NOT EXISTS Comments (" +
-                "commenter VARCHAR(32) NOT NULL," +
-                "p_id VARCHAR(32) NOT NULL," +
-                "contents VARCHAR(1024) NOT NULL" +
-                "time_sent BIGINT NOT NULL," +
-                "FOREIGN KEY(commenter) REFERENCES Users(username)" +
-                    "ON DELETE CASCADE " +
-                    "ON UPDATE CASCADE," +
-                "FOREIGN KEY(p_id) REFERENCES POSTS(p_id) ON DELETE CASCADE" +
-            ")"
+                "CREATE TABLE IF NOT EXISTS PagePosts (" +
+                        "p_id INTEGER NOT NULL," +
+                        "pg_name VARCHAR(32) NOT NULL," +
+                        "FOREIGN KEY(p_id) REFERENCES POSTS(p_id) ON DELETE CASCADE," +
+                        "FOREIGN KEY(pg_name) REFERENCES Pages(pg_name)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE" +
+                        ")"
+        );
+        stmt.execute();
+
+        stmt = db.prepareStatement(
+                "CREATE TABLE IF NOT EXISTS PageFollowers (" +
+                        "username VARCHAR(32) NOT NULL," +
+                        "pg_name VARCHAR(32) NOT NULL," +
+                        "FOREIGN KEY(username) REFERENCES Users(username)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE," +
+                        "FOREIGN KEY(pg_name) REFERENCES Pages(pg_name)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE" +
+                        ")"
+        );
+        stmt.execute();
+
+        stmt = db.prepareStatement(
+                "CREATE TABLE IF NOT EXISTS Messages (" +
+                        "sender VARCHAR(32) NOT NULL," +
+                        "receiver VARCHAR(32) NOT NULL," +
+                        "message VARCHAR(1024) NOT NULL," +
+                        "time_sent BIGINT NOT NULL," +
+                        "time_seen BIGINT," +
+                        "FOREIGN KEY(sender) REFERENCES Users(username)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE," +
+                        "FOREIGN KEY(receiver) REFERENCES Users(username)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE" +
+                        ")"
+        );
+        stmt.execute();
+
+        stmt = db.prepareStatement(
+                "CREATE TABLE IF NOT EXISTS Comments (" +
+                        "commenter VARCHAR(32) NOT NULL," +
+                        "p_id INTEGER NOT NULL," +
+                        "contents VARCHAR(1024) NOT NULL," +
+                        "time_sent BIGINT NOT NULL," +
+                        "FOREIGN KEY(commenter) REFERENCES Users(username)" +
+                        "ON DELETE CASCADE " +
+                        "ON UPDATE CASCADE," +
+                        "FOREIGN KEY(p_id) REFERENCES POSTS(p_id) ON DELETE CASCADE" +
+                        ")"
         );
         stmt.execute();
 
--- app/src/test/java/com/sicknasty/business/AccessPostsTest.java
+++ app/src/test/java/com/sicknasty/business/AccessPostsTest.java
@@ -13,33 +13,47 @@ import com.sicknasty.persistence.PostPersistence;
 import com.sicknasty.persistence.exceptions.DBPostIDExistsException;
 import com.sicknasty.persistence.stubs.PostPersistenceStub;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
 
 public class AccessPostsTest {
 
 
-    private AccessPosts posts = new AccessPosts(new PostPersistenceStub());
+    private AccessPosts accessPostStub;
+    private AccessPosts accessPostMock;
+
+    @Before
+    public final void setup(){
+
+        PostPersistence postPersistenceMock,postPersistenceStub;
+
+        postPersistenceStub = new PostPersistenceStub();
+        postPersistenceMock = mock(PostPersistence.class);
+
+        accessPostStub = new AccessPosts(postPersistenceStub);
+        accessPostMock = new AccessPosts(postPersistenceMock);
+    }
 
     @Test
     public void testGetPostsByPage() throws NoValidPageException,ChangeNameException, PasswordErrorException, UserCreationException, ChangeUsernameException, DBPostIDExistsException {
         User newUser = new User("hello", "helloo", "hellooooooo");
         Page page = new PersonalPage(newUser);
         Post post = new Post("this is a test",null,null,1, 1, page);
-        //insert the post
-        assertTrue("the post was not properly inserted", posts.insertPost(post));
+        assertTrue("the post was not properly inserted", accessPostStub.insertPost(post));
 
-        //try searching for the post in a different page that what it was placed in
         Page newPage=new PersonalPage(new User("test", "test", "testing1234"));
-        ArrayList<Post> test = posts.getPostsByPage(newPage);
+        ArrayList<Post> test = accessPostStub.getPostsByPage(newPage);
         assertFalse("the post should not be in this list, it was posted to a different page", test.contains(post));
 
-        //Check if the post is in the page we inserted into's list of posts
-        assertTrue("the post is not in the pages list of posts", posts.getPostsByPage(page).contains(post));
+        assertTrue("the post is not in the pages list of posts", accessPostStub.getPostsByPage(page).contains(post));
     }
 
     @Test
@@ -47,7 +61,7 @@ public class AccessPostsTest {
         User newUser = new User("hello", "helloo", "hellooooooo");
         Page page = new PersonalPage(newUser);
         Post post = new Post("this is a test",null,null,1, 1, page);
-        assertTrue("the post was not properly inserted", posts.insertPost(post));
+        assertTrue("the post was not properly inserted", accessPostStub.insertPost(post));
     }
 
     @Test
@@ -55,16 +69,52 @@ public class AccessPostsTest {
         User newUser = new User("hello", "helloo", "hellooooooo");
         Page page = new PersonalPage(newUser);
         Post post = new Post("this is a test",null,null,1, 1, page);
-        assertTrue("the post was not properly inserted", posts.insertPost(post));
 
+        assertTrue("the post was not properly inserted", accessPostStub.insertPost(post));
 
-        //try deleting the post
-        assertTrue("the deletion failed from the persistence stub", posts.deletePost(post));
-        //try deleting the same post again (should fail)
-        assertFalse("We somehow deleted the same post twice", posts.deletePost(post));
+        assertTrue("the deletion failed from the persistence stub", accessPostStub.deletePost(post));
+
+        assertFalse("We somehow deleted the same post twice", accessPostStub.deletePost(post));
 
         Post aDifferentPost = new Post("this is a test",newUser,null,1, 1, page);
-        //try to delete a post that has not been added
-        assertFalse("deleted a post that was not inserted to the db", posts.deletePost(aDifferentPost));
+
+        assertFalse("deleted a post that was not inserted to the db", accessPostStub.deletePost(aDifferentPost));
+    }
+    @Test
+    public void deleteNullPost()
+    {
+        //normal unit test
+    }
+    @Test
+    public void testGetPostsByFilter()
+    {
+        //use mockito
+    }
+
+    @Test
+    public void testInsertedFields()
+    {
+
+    }
+
+    @Test
+    public void testDeleteById()
+    {
+
+
+    }
+
+    @Test
+    public void testGetDeleted()
+    {
+
+    }
+
+
+    @After
+    public final void tearDown()
+    {
+        accessPostMock = null;
+        accessPostStub = null;
     }
 }
